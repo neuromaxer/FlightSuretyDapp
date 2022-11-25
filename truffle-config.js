9b090719ee6e09c867df3a89290d27ae700c14cf
neuromaxer <neuromaxed@gmail.com>
2022-11-25 08:53:17 +0100

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic =
+    "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+module.exports = {
+    networks: {
+        development: {
+            provider: function () {
+                return new HDWalletProvider(
+                    mnemonic,
+                    "http://127.0.0.1:7545/",
+                    0,
+                    50
+                );
+            },
+        },
+    },
+    compilers: {
+        solc: {
+            version: "^0.8.16",
+        },
+    },
+};
