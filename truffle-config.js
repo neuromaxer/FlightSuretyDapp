3c499a2910289cf5c5bc781dffd6f61814b70c01
neuromaxer <neuromaxed@gmail.com>
2022-11-30 02:51:42 +0100

    add configs

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,27 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic =
-    "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
 module.exports = {
     networks: {
-        development: {
-            provider: function () {
-                return new HDWalletProvider(
-                    mnemonic,
-                    "http://127.0.0.1:7545/",
-                    0,
-                    50
-                );
-            },
+        test: {
+            network_id: 8545,
+            accounts: 50,
+        },
+        develop: {
+            network_id: 7545,
+            accounts: 50,
         },
     },
     compilers: {
         solc: {
-            version: "^0.8.16",
+            version: "^0.8.16", // Fetch exact version from solc-bin (default: truffle's version)
+            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+            settings: {
+                // See the solidity docs for advice about optimization and evmVersion
+                viaIR: true,
+                optimizer: {
+                    enabled: true,
+                    runs: 200,
+                },
+                //  evmVersion: "byzantium"
+            },
         },
     },
 };
